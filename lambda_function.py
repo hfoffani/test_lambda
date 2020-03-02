eba94d38b0a98e724d6ed15c42869859b4b77f49
hfoffani <hfoffani@gmail.com>
2020-03-02 20:27:30 +0100

    Change text

--- lambda_function.py
+++ lambda_function.py
@@ -9,7 +9,7 @@ from ftplib import FTP_TLS
 
 
 loremipsum = """
-4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+5 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
