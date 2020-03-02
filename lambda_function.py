2bc6d1880089dfeb83551f30e65f6da04ab3275d
hfoffani <hfoffani@gmail.com>
2020-03-02 20:18:41 +0100

    Read from FTP
    Uses a config to load parameters.

--- lambda_function.py
+++ lambda_function.py
@@ -5,9 +5,11 @@ import os
 import watchtower, logging
 import boto3
 from io import BytesIO
+from ftplib import FTP_TLS
+
 
 loremipsum = """
-1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
@@ -30,22 +32,43 @@ cl_handler = watchtower.CloudWatchLogHandler(
     stream_name="logger", )
 logger.addHandler(cl_handler)
 
-def process(country):
+
+def read_ftp(ftpparams):
+    with FTP_TLS(ftpparams['host']) as ftps:
+        ftps.login(ftpparams['user'], ftpparams['password'])
+        ftps.prot_p()
+
+        ftps.cwd(ftpparams['dir'])
+        filename = ftpparams['finput']
+
+        with BytesIO() as inpbin:
+            ftps.retrbinary('RETR %s' % filename, inpbin.write)
+            return inpbin.getvalue().decode('utf-8-sig')
+
+
+def process(params):
+    country = params['country']
     logger.warn(f"Hello from {country}")
+
+    # read from FTP
+    txt = read_ftp(params)
+    logger.warn(f"Got {len(txt)} chars.")
 
     # write to S3
     filename = "lorenipsum.txt"
 
     s3 = session.client('s3')
     with BytesIO(loremipsum.encode()) as bio:
-        s3.upload_fileobj( bio, "herchu-lambda", filename )
+        s3.upload_fileobj( bio, params["bucket"], filename )
+    logger.warn(f"Wrote to S3.")
 
     cl_handler.flush()
     return f"OK {country}"
 
+
 def lambda_handler(event, context):
 
-    res = process(event["country"])
+    res = process(event)
 
     return {
         'statusCode': 200,
--- prog.py
+++ prog.py
@@ -1,7 +1,12 @@
 
 import os
+import configparser
 
 from lambda_function import process
 
 if __name__ == '__main__':
-    process("AUS")
+
+    config = configparser.ConfigParser()
+    config.read(['config.cfg'])
+
+    process(config['DEFAULT'])
